74441925831426a34af39eb9732d6337505cc6a9
jose manuel alfonso <jose.manuelchema111@gmail.com>
2019-02-22 12:40:37 +0100

    Comienzo datos clientes

--- db/db.sql
+++ db/db.sql
@@ -1,6 +1,21 @@
+CREATE DATABASE IF NOT EXISTS basedatos;
+
 use basedatos;
 
-CREATE TABLE productos(
+
+/* Tabla usuarios */
+CREATE TABLE IF NOT EXISTS usuarios(
+    usuario_id      int(10) auto_increment,
+    usuario         varchar(50),
+    contrasena      varchar(255),
+    nombre          varchar(125),
+    email           varchar(150),
+    CONSTRAINT pk_pkusuarios PRIMARY KEY (usuario_id)
+)Engine=InnoDB;
+
+
+/* Tabla productos */
+CREATE TABLE IF NOT EXISTS productos(
     producto_id int(11) auto_increment,
     codigo      char(8),
     nombre      varchar(32),
@@ -8,3 +23,19 @@ CREATE TABLE productos(
     precio      decimal(10,3),
     CONSTRAINT pk_pkproductos PRIMARY KEY (producto_id)
 )Engine=InnoDB;
+
+
+/* Tabla clientes */
+CREATE TABLE IF NOT EXISTS clientes(
+    cliente_id  int(11) auto_increment,
+    nombre      varchar(32),
+    cif         char(9) UNIQUE,
+    direccion   varchar(32),
+    poblacion   varchar(32),
+    provincia   varchar(32),
+    cp          char(5),
+    telefono    char(11),
+    email       varchar(64) UNIQUE,
+    CONSTRAINT pk_pkclientes PRIMARY KEY (cliente_id)
+)Engine=InnoDB;
+
